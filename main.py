fc832da8f1451cc1f4651b2cc8d55df70570b2a8
jestemAria <radmehr.krash@gmail.com>
2021-11-05 15:21:16 +0330

    Variables changed to better ones, also alternitive algorithms added as comment.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from cv2 import cv2 as cv
 
 cap = cv.VideoCapture(0)
-fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+fore_ground_back_ground = cv.bgsegm.createBackgroundSubtractorMOG()
+
+# Alternatively, you can use the following:
+# fore_ground_back_ground = cv.bgsegm.createBackgroundSubtractorGMG()
+# fore_ground_back_ground = cv.createBackgroundSubtractorMOG2()
 
 while True:
     ret, frame = cap.read()
     if frame is None:
         break
 
-    fgmask = fgbg.apply(frame)
+    fore_ground_maks = fore_ground_back_ground.apply(frame)
 
     cv.imshow('Frame', frame)
-    cv.imshow('FG Mask Frame', fgmask)
+    cv.imshow('FG Mask Frame', fore_ground_maks)
 
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
